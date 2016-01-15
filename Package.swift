4ccff6189f0a1c6dbb4597d21749b2735790ef43
Brandon Evans <brandon@brandonevans.ca>
2016-01-15 00:48:00 -0700

    Update OpenSSL

--- Package.swift
+++ Package.swift
@@ -4,9 +4,9 @@ let package = Package(
     name: "SwiftAPI",
     dependencies: [
         .Package(url: "https://github.com/interstateone/WebSocket.git", Version(0,1,2)),
+        .Package(url: "https://github.com/interstateone/OpenSSL.git", Version(0,1,6)),
         .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/HTTP.git", Version(0,1,2)),
-        .Package(url: "https://github.com/interstateone/OpenSSL.git", Version(0,1,6))
         .Package(url: "https://github.com/tyrone-sudeium/JSONCore.git", Version(1,0,0))
     ],
     targets: [
