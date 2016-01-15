6daf31e3ca7bff3f30d05cb00caf386cbe3effa2
Brandon Evans <brandon@brandonevans.ca>
2016-01-15 00:46:36 -0700

    Update WebSocket with improved client implementation

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftAPI",
     dependencies: [
-        .Package(url: "https://github.com/interstateone/WebSocket.git", Version(0,1,1)),
+        .Package(url: "https://github.com/interstateone/WebSocket.git", Version(0,1,2)),
         .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/HTTP.git", Version(0,1,2)),
         .Package(url: "https://github.com/interstateone/OpenSSL.git", Version(0,1,6))
