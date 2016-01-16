00b3c11ca0e8f811200daaee9bd908246544fd52
Brandon Evans <brandon@brandonevans.ca>
2016-01-15 21:48:29 -0700

    Remove example targets from Package.swift

--- Package.swift
+++ Package.swift
@@ -1,27 +1,27 @@
 import PackageDescription
 
+#if os(OSX)
+    let openSSLURL = "https://github.com/Zewo/COpenSSL-OSX.git"
+#else
+    let openSSLURL = "https://github.com/Zewo/COpenSSL.git"
+#endif
+
 let package = Package(
     name: "SwiftAPI",
     dependencies: [
         .Package(url: "https://github.com/interstateone/WebSocket.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/interstateone/OpenSSL.git", majorVersion: 0, minor: 1),
+        .Package(url: openSSLURL, majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/HTTP.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/tyrone-sudeium/JSONCore.git", majorVersion: 1, minor: 0)
     ],
     targets: [
-        Target(
-            name: "Example",
-            dependencies: [.Target(name: "SlackAPI")]),
-        Target(
-            name: "SlackAPI",
-            dependencies: [.Target(name: "Zeal")]),
-        Target(name: "Zeal"),
-        Target(
-            name: "ZealExample",
-            dependencies: [.Target(name: "Zeal")]),
-        Target(
-            name: "WebSocketExample",
-            dependencies: [.Target(name: "Zeal")])
+        Target(name: "SlackAPI", dependencies: [.Target(name: "Zeal")]),
+        Target(name: "Zeal")
     ]
 )
