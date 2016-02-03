a306c832a87cff797b2e9478d08eb0e515ff243f
Dave Anderson <david.anderson@robotsandpencils.com>
2016-02-02 17:39:26 -0700

    Use Zeal as a Package instead of as a Target As a target Zeal wasn't being linked in properly

--- Package.swift
+++ Package.swift
@@ -21,15 +21,13 @@ let package = Package(
         .Package(url: "https://github.com/interstateone/OpenSSL.git", majorVersion: 0, minor: 1),
         .Package(url: openSSLURL, majorVersion: 0, minor: 1),
 
-        // Zeal, whose source is part of this repo, depends on HTTPParser and CHTTPParser...
+        .Package(url: "https://github.com/interstateone/Zeal.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 1),
-        .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 1),
         .Package(url: "https://github.com/tyrone-sudeium/JSONCore.git", majorVersion: 1, minor: 0)
     ],
     targets: [
-        Target(name: "SlackAPI", dependencies: [.Target(name: "Zeal")]),
-        Target(name: "Zeal")
+        Target(name: "SlackAPI")
     ]
 )
